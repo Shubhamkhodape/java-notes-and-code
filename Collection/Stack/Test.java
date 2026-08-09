91fe53351e97933a298fd210c82874b340bcfcfc
Shubham Khodape <skhodape@gmail.com>
2026-08-09 13:57:18 +0530

    Stack in Collection Framework

---
+++ Collection/Stack/Test.java
@@ -0,0 +1,35 @@
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Stack;
+
+
+public class Test {
+    public static void main(String[] args) {
+    
+        Stack<Integer> stack = new Stack<>();
+        stack.add(11);
+        stack.add(88);
+        stack.add(15);
+        stack.add(3);
+
+        stack.push(90);
+        stack.pop();
+        stack.peek();
+        System.out.println(stack.search(15));
+        System.out.println(stack.empty());
+
+        System.out.println(stack);
+
+        Iterator<Integer> iterator = stack.iterator();
+        while(iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+
+        Collections.sort(stack);
+        System.out.println(stack);
+
+
+
+    }
+}
